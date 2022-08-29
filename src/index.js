8c6f576484736626338c2f474689f32f50f32009
Matheus <matheus.augustoreal@gmail.com>
2022-08-29 11:41:55 -0300

    Listing statement

--- src/index.js
+++ src/index.js
@@ -5,12 +5,12 @@ const app = express()
 
 app.use(express.json())
 
-const custumers = []
+const customers = []
 
 app.post("/account", (request, response) => {
   const { cpf, name } = request.body
 
-  const customerAlreadyExists = custumers.some(
+  const customerAlreadyExists = customers.some(
     (custumer) => custumer.cpf === cpf
   )
 
@@ -18,7 +18,7 @@ app.post("/account", (request, response) => {
     return response.status(400).json({ error: 'Customer already exists' })
   }
 
-  custumers.push({
+  customers.push({
     cpf,
     name,
     id: uuidv4(),
@@ -26,6 +26,14 @@ app.post("/account", (request, response) => {
   })
 
   return response.status(201).send()
+})
+
+app.get("/statement/:cpf", (request, response) => {
+  const { cpf } = request.params
+
+  const customer = customers.find(customer => customer.cpf === cpf)
+
+  return response.json(customer.statement)
 })
 
 app.listen(3333)

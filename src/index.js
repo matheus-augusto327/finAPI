77cab44612aeca9500404b3b10e9ad96286a83e9
Matheus <matheus.augustoreal@gmail.com>
2022-08-30 10:22:23 -0300

    Creating Account Deposit

--- src/index.js
+++ src/index.js
@@ -7,11 +7,25 @@ app.use(express.json())
 
 const customers = []
 
+function verifyIfExistsAccountCPF(request, response, next) {
+  const { cpf } = request.headers;
+
+  const customer = customers.find(customer => customer.cpf === cpf)
+
+  if(!customer) {
+    return response.status(404).json({ error: 'Customer not found'})
+  }
+
+  request.customer = customer
+
+  return next()
+}
+
 app.post("/account", (request, response) => {
   const { cpf, name } = request.body
 
   const customerAlreadyExists = customers.some(
-    (custumer) => custumer.cpf === cpf
+    (customer) => customer.cpf === cpf
   )
 
   if(customerAlreadyExists) {
@@ -25,15 +39,32 @@ app.post("/account", (request, response) => {
     statement: []
   })
 
+  console.log(customers)
+
   return response.status(201).send()
 })
 
-app.get("/statement/:cpf", (request, response) => {
-  const { cpf } = request.params
-
-  const customer = customers.find(customer => customer.cpf === cpf)
+app.get("/statement", verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request
 
   return response.json(customer.statement)
+})
+
+app.post("/deposit", verifyIfExistsAccountCPF, (request, response) => {
+  const { description, amount } = request.body
+
+  const { customer } = request;
+
+  const statementOperation = {
+    description,
+    amount,
+    created_at: new Date(),
+    type: "credit"
+  }
+
+  customer.statement.push(statementOperation)
+
+  return response.status(201).send()
 })
 
 app.listen(3333)
